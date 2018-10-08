425c3e7024edc4f46e893773d8b19025ab7b6545
lanpn <lanpn@vng.com.vn>
2018-10-08 10:35:36 +0700

    Add numbers to labels

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
-python3 test.py --model-path models/gg15_v1/best.pth\
-	--test-manifest /home/common/corpora/speech/dung_speech/val_manifest.csv\
-	--verbose --cuda --decoder beam\
+python3 test.py --model-path models/gg15_v4/best.pth\
+	--test-manifest /home/common/corpora/speech/vov/full_manifest.csv\
+	--verbose --cuda --decoder greedy\
 	--beam-width 100\
-	--lm-path lm/vn_1.arpa\
+	--lm-path lm/vn_1.lm\
 	--alpha 0.3\
-	--lm-workers 4
+	--lm-workers 4\
+	> logs/gg15_v4-nolm.log
--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
-python3 train.py --cuda --epochs 50 --checkpoint\
-	--train-manifest /home/common/corpora/speech/dung_speech/train_manifest.csv\
-	--val-manifest /home/common/corpora/speech/dung_speech/val_manifest.csv\
-	--batch-size 10 --hidden-size 500 --lr 3e-5 --augment\
-	--hidden-layers 4 --learning-anneal 1.1\
-	--model-path models/gg15_v1/best.pth\
-	--save-folder models/gg15_v1/\
+python3 train.py --cuda --epochs 20 --checkpoint\
+	--train-manifest /home/common/corpora/speech/dung_speech/full_manifest.csv\
+	--val-manifest /home/common/corpora/speech/vov/full_manifest.csv\
+	--batch-size 15 --hidden-size 300 --lr 1e-4 --augment\
+	--hidden-layers 5 --learning-anneal 1.0\
+	--model-path models/gg15_v4/best.pth\
+	--save-folder models/gg15_v4/\
 	--labels-path labels-vi.json\
-	--continue-from models/gg15_v1/best.pth --finetune
+	--continue-from models/gg15_v4/best.pth --finetune
